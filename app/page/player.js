986ca65354a3ea5085e5d46eb2a362fadb4389f6
sundawei2018 <dsun2016@gmail.com>
2018-11-09 01:37:31 -0500

    testing

--- app/page/player.js
+++ app/page/player.js
@@ -9,7 +9,7 @@ let Player = React.createClass({
 		return {
 			progress: 0,
 			volume: 0,
-			isPlay: true,
+			isPlay: true
 			// leftTime: ''
 		}
 	},	
@@ -113,49 +113,6 @@ let Player = React.createClass({
 						</div>
 					</div>
 			</div>
-            // <div className="player-page">
-            //     <h1 className="caption">我的私人音乐坊</h1>
-            //     <div className="mt20 row">
-            //     	<div className="controll-wrapper">
-            //     		<h2 className="music-title">{this.props.currentMusitItem.title}</h2>
-            //     		<h3 className="music-artist mt10">{this.props.currentMusitItem.artist}</h3>
-            //     		<div className="row mt20">
-            //     			<div className="left-time -col-auto">-{this.state.leftTime}</div>
-            //     			<div className="volume-container">
-            //     				<i className="icon-volume rt" style={{top: 5, left: -5}}></i>
-            //     				<div className="volume-wrapper">
-			// 		                <Progress
-			// 							progress={this.state.volume}
-			// 							onProgressChange={this.changeVolumeHandler}
-			// 							barColor='#aaa'
-			// 		                >
-			// 		                </Progress>
-            //     				</div>
-            //     			</div>
-            //     		</div>
-            //     		<div style={{height: 10, lineHeight: '10px'}}>
-			//                 <Progress
-			// 					progress={this.state.progress}
-			// 					onProgressChange={this.changeProgressHandler}
-			//                 >
-			//                 </Progress>
-            //     		</div>
-            //     		<div className="mt35 row">
-            //     			<div>
-	        //         			<i className="icon prev" onClick={this.prev}></i>
-	        //         			<i className={`icon ml20 ${this.state.isPlay ? 'pause' : 'play'}`} onClick={this.play}></i>
-	        //         			<i className="icon next ml20" onClick={this.next}></i>
-            //     			</div>
-            //     			<div className="-col-auto">
-            //     				<i className={`icon repeat-${this.props.repeatType}`} onClick={this.changeRepeat}></i>
-            //     			</div>
-            //     		</div>
-            //     	</div>
-            //     	<div className="-col-auto cover">
-            //     		<img src={this.props.currentMusitItem.cover} alt={this.props.currentMusitItem.title}/>
-            //     	</div>
-            //     </div>
-            // </div>
         );
     }
 });
